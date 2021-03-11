62d79edb4260a24898654bdd2390f82449c0830a
Roman Matsa <matsars@gmail.com>
2021-03-11 15:31:05 +0200

    add http requests functionality

--- app/src/main/java/com/example/weather/MainActivity.java
+++ app/src/main/java/com/example/weather/MainActivity.java
@@ -4,10 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.JsonReader;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
     private EditText msgCity;
     private Button buttonByCity;
     private Button buttonByLocation;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,13 +50,26 @@ public class MainActivity extends AppCompatActivity {
             msgTemperature.setText(getText(R.string.msg_wrong_city));
             return;
         }
+
         String request = formatRequestWithCity(city);
-        doRequest(request);
+        Log.d("myTag", request);
+        String response = doRequest(request);
+        if (response != null) {
+            int len = response.length();
+            Log.d("myTag", "len:" + len);
+        } else {
+            Log.d("myTag", "response == null");
+            return;
+        }
 
-        //msgTemperature.setText("OK");
-
+        String formattedOutput = getTemperature(response);
+        if (formattedOutput == null) {
+            return;
+        }
+        String newMessage = getText(R.string.default_message) + " " + formattedOutput + " \u00B0C";
+        msgTemperature.setText(newMessage);
     }
-    
+
     public void onClickByLocation(View view) {
 
     }
@@ -49,8 +79,6 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
-
-
     private String formatRequestWithCity(String city) {
         String api_url = getText(R.string.weather_entry_point).toString();
         String key = getText(R.string.weather_key).toString();
@@ -58,13 +86,66 @@ public class MainActivity extends AppCompatActivity {
         return api_url_all;
     }
 
-    private void doRequest(String request) {
-        AsyncTask.execute(new Runnable() {
+    private String getTemperature(String response) {
+        String ret = null;
+
+        try {
+            JSONObject json = new JSONObject(response);
+            JSONObject main  = json.getJSONObject("main");
+            ret = main.getString("temp");
+            Log.d("myTag", "temperature = " + ret);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return ret;
+    }
+
+    private String doRequest(String request) {
+        final String[] respond = {null};
+        Thread t = new Thread(new Runnable() {
             @Override
             public void run() {
-                // All your networking logic
-                // should be here
+                BufferedReader reader = null;
+                InputStream stream = null;
+                HttpsURLConnection connection = null;
+                try {
+                    URL weatherEndpoint = new URL(request);
+                    connection = (HttpsURLConnection) weatherEndpoint.openConnection();
+                    connection.setRequestProperty("User-Agent", getText(R.string.app_name).toString());
+                    connection.setRequestMethod("GET");
+                    connection.setReadTimeout(10000);
+                    connection.connect();
+
+                    if (connection.getResponseCode() == 200) {
+                        stream = connection.getInputStream();
+                        reader= new BufferedReader(new InputStreamReader(stream));
+                        StringBuilder buf = new StringBuilder();
+                        String line;
+                        while ((line=reader.readLine()) != null) {
+                            buf.append(line).append("\n");
+                        }
+                        respond[0] = buf.toString();
+
+                    } else {
+                        Log.d("myTag", "not 200");
+                    }
+
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         });
+        t.start();
+        try {
+            t.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return respond[0];
     }
+
 }
